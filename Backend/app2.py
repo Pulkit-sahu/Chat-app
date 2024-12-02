79f299ed6a2dac16767a97a7035a00c648413db6
Pulkit Sahu <93196038+Pulkit-sahu@users.noreply.github.com>
2024-12-02 11:56:47 +0530

    Update app2.py
    debug

--- Backend/app2.py
+++ Backend/app2.py
@@ -273,6 +273,6 @@ def handle_disconnect():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app,allow_unsafe_werkzeug=True, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app,allow_unsafe_werkzeug=True, host='0.0.0.0', port=5000)
 
 
